42d347bdd2869e0276c94bfae02fca92115bc03e
eddychen86 <eddychen101020@gmail.com>
2022-02-21 12:34:20 +0800

    product

--- shopping-cart/src/views/ProductView.vue
+++ shopping-cart/src/views/ProductView.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
+  <div class="row">
     <template v-for="product in products" :key="product">
-      {{ product.name }} <br>
-      {{ product.category}} <br>
+      <div class="product-box">
+        <div class="image-box">
+          <img :src="parseImgPath(product.img_path)">
+        </div>
+        <h4>{{ product.brand }}<small>{{ product.category }}</small></h4>
+        <h3>{{ product.name }}</h3>
+        <p>售價：{{ product.price }}</p>
+      </div>
     </template>
   </div>
 </template>
@@ -20,10 +26,43 @@
       axios
         .get(`http://35.234.34.149/products?user_id=1`)
         .then(response => this.products = response.data)
+    },
+    methods: {
+      parseImgPath: function(path) {
+        return `http://35.234.34.149${path}`;
+      }
     }
   }
 </script>
 
 <style scoped>
-
+  * {
+    font-family: '微軟正黑體';
+  }
+  .row {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 80%;
+    margin: auto;
+  }
+  .product-box {
+    border: 1px solid;
+    min-width: 350px;
+    width: 24%;
+    text-align: center;
+    margin: 15px;
+    cursor: pointer;
+    display: block;
+  }
+  .image-box {
+    width: 100%;
+    height: 250px;
+    margin: auto;
+  }
+  img {
+    display: block;
+    width: 50%;
+    margin: auto;
+  }
 </style>
